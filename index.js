2275a44a463d751fba5c31c352292da4a70964f2
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-09 10:41:12 +0300

    commit tree b3e5efff0742a23ddff1dd9371f5d877477baf24

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let  currentUser = "James Mburu"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} ,Welcome to Flatbook!`                                        //"Welcome to Flatbook, "
+// welcomeMessage = ''
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+//`WELCOME TO FLATBOOK, ${currentUser}, WELCOME TO FLATBOOK!`
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`
